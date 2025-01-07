9f40bd5a64895b16ec8041277f2778d9aad1ca82
jeremy-brunel <jeremy.brunel@bashroom.com>
2025-01-07 09:50:38 +0100

    first commit

---
+++ bench/BenchmarkTask.java
@@ -0,0 +1,8 @@
+public interface BenchmarkTask {
+    /**
+     * Perform a benchmarked task with arbitrary arguments.
+     * @param args variable-length array of Objects (any type).
+     * @return an Object that can represent any result type.
+     */
+    Object perform(Object... args);
+}
